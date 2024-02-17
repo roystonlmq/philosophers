c7b69211d52f78fe54efaa537fb025f8323a1f76
roylee <roystonlmq@gmail.com>
2024-02-17 11:58:42 +0800

    first commit

---
+++ philo/includes/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 17:58:36 by roylee            #+#    #+#             */
+/*   Updated: 2024/02/09 18:05:11 by roylee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h> // printf
+# include <string.h> // memset
+# include <stdlib.h> // malloc, free
+# include <pthread.h> // pthread_create, pthread_join, pthread_mutex_init, 
+//pthread_mutex_lock, pthread_mutex_unlock, pthread_mutex_destroy
+# include <unistd.h> // usleep, write
+# include <sys/time.h> // gettimeofday
+
+
+#endif
