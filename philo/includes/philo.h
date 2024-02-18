c709e44c1f39c5d155d806a9091cf866f590c93a
roylee <roylee@iamagod.sg>
2024-02-18 14:52:11 +0800

    add free, time, init, start

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 17:58:36 by roylee            #+#    #+#             */
-/*   Updated: 2024/02/18 13:23:27 by roylee           ###   ########.fr       */
+/*   Updated: 2024/02/18 14:37:14 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,51 @@
 # include <sys/time.h> // gettimeofday
 
 typedef struct s_philo	t_philo;
+typedef pthread_mutex_t	t_mtx;
+
+typedef enum e_state
+{
+	THINK,
+	EAT,
+	SLEEP,
+	FORK1,
+	FORK2,
+	DIED,
+}	t_state;
 
 typedef struct	s_prog
 {
-	t_philo	*philos;
-	long	ttd;
-	long	tte;
-	long	tts;
-	int		philo_nbr;
-	int		eat_limit;
-	int		dead;
+	pthread_t	*threads;
+	t_philo		*philos;
+	t_fork		*forks;
+	t_mtx		write_mtx;
+	t_mtx		app_mtx;
+	long		ttd;
+	long		tte;
+	long		tts;
+	long		start;
+	int			philo_nbr;
+	int			eat_limit;
+	int			end;
 }	t_prog;
 
 typedef struct	s_fork
 {
-	
-}
+	t_mtx		mtx;
+	int			id;
+}	t_fork;
 
 struct	s_philo
 {
-	int		id;
-	t_fork	*left;
-	t_fork	*right;
-	
-}
+	pthread_t	tid;
+	t_mtx		philo_mtx;
+	t_fork		*left;
+	t_fork		*right;
+	t_prog		*app;
+	long		last_meal;
+	int			id;
+	int			eat_count;
+}	t_philo;
 
 /*
 srcs/lib/*.c
@@ -56,6 +77,11 @@ int		ft_isdigit(int c);
 int		ft_isnbr(const char *s);
 int		ft_strlen(const char *s);
 
+/*
+time.c
+*/
+int		ft_usleep(long time);
+long	get_time(void);
 
 /*
 error.c
@@ -65,6 +91,12 @@ void	exception(const char *s);
 /*
 init.c
 */
+void	init_app(t_prog *app, int ac, char **av);
+
+/*
+free.c
+*/
+
 
 /*
 parser.c
