d02d2b75831ecb9f953870af820c3832ed69f1ec
roylee <roylee@iamagod.sg>
2024-02-18 14:52:32 +0800

    add the c files

---
+++ philo/srcs/time.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/18 14:24:40 by roylee            #+#    #+#             */
+/*   Updated: 2024/02/18 14:29:14 by roylee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+long	get_time(void)
+{
+	struct timeval	tv;
+
+	if (gettimeofday(&tv, NULL) == -1)
+		exception("gettimeofday failed\n");
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+int		ft_usleep(long time)
+{
+	long	start;
+
+	start = get_time();
+	while (get_time() - start < time)
+		usleep(100);
+	return (0);
+}
