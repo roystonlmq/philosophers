a62c491024783c9b34b58aa000e823f18ee1e5b5
roylee <roylee@iamagod.sg>
2024-02-18 12:49:51 +0800

    add parser file

---
+++ philo/srcs/parser.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/18 12:43:35 by roylee            #+#    #+#             */
+/*   Updated: 2024/02/18 12:47:53 by roylee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*
+check_input:
+./philo philo_nbr time_to_die time_to_eat time_to_sleep [optional: eat_limit]
+./philo	5	800			200			200				7
+*/
+void	check_input(int ac, char **av, t_prog *app)
+{
+	int	philo_nbr;
+	int	ttd;
+	int	tte;
+	int	tts;
+	int	eat_limit;
+
+	
+}
