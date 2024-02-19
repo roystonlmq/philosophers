61e21a2b10af85b7e552df2ead7175d8ff4abafc
roylee <roylee@iamagod.sg>
2024-02-19 21:47:19 +0800

    add state function and all ate

--- philo/srcs/start.c
+++ philo/srcs/start.c
@@ -6,35 +6,61 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 14:50:41 by roylee            #+#    #+#             */
-/*   Updated: 2024/02/18 15:41:11 by roylee           ###   ########.fr       */
+/*   Updated: 2024/02/19 21:43:09 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int		check_dead(t_philo *philo)
+static int	all_ate(t_philo *philo)
+{
+	int	i;
+
+	i = -1;
+	while (++i < philo->app->philo_nbr)
+	{
+		if (philo->app->philos[i].eat_count < philo->app->meals)
+			return (ALIVE);
+	}
+	philo->app->end = 1;
+}
+
+/*
+checks if philo is dead or all philos ate
+*/
+int		ft_state(t_philo *philo)
 {
 	pthread_mutex_lock(philo->app->dead);
-	if (philo->app->end)
+	pthread_mutex_lock(philo->app->meal);
+	if (get_time() - philo->last_eat >= philo->ttd && philo->state != EATING)
 	{
+		log(philo, "died");
+		philo->app->end = 1;
+		pthread_mutex_unlock(philo->app->meal);
 		pthread_mutex_unlock(philo->app->dead);
-		return (1);
+		return (DIED);
 	}
+	if (all_ate(philo) == ALL_ATE)
+	{
+		philo->app->end = 1;
+		pthread_mutex_unlock(philo->app->meal);
+		pthread_mutex_unlock(philo->app->dead);
+		return (ALL_ATE);
+	}
+	pthread_mutex_unlock(philo->app->meal);
 	pthread_mutex_unlock(philo->app->dead);
-	return (0);
+	return (ALIVE);
 }
 
 void	start_routine(void *arg)
 {
-	t_philo	*philo;
+	t_philo *philo;
+	int		i;
 
-	philo = (t_philo *)arg;
 	if (philo->id % 2 == 0)
 		ft_usleep(100);
-	while (!check_dead(philo))
+	while (ft_state(philo) != DIED || ft_state(philo) != ALL_ATE)
 	{
-		if (philo->app->end)
-			break ;
 		eat(philo);
 		sleep(philo);
 		think(philo);
@@ -48,6 +74,9 @@ void	start(t_prog *app)
 	i = -1;
 	while (++i < app->philo_nbr)
 	{
-		pthread_create(&app->philo[i].tid, NULL, &philo, &app->philo[i]);
+		if (pthread_create(&app->philo[i].tid, NULL, &start_routine, 
+			&app->philos[i]) != 0)
+			thread_exception()
+		
 	}
 }
