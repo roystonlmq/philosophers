7fb4048c48457ba266259744aaa3eac15e375a94
roylee <roylee@iamagod.sg>
2024-02-19 21:50:26 +0800

    add thread_exception

--- philo/srcs/error.c
+++ philo/srcs/error.c
@@ -6,14 +6,28 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 12:37:49 by roylee            #+#    #+#             */
-/*   Updated: 2024/02/18 12:41:44 by roylee           ###   ########.fr       */
+/*   Updated: 2024/02/19 21:45:53 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	exception(const char *s)
+void	exception(const char *s, t_prog *int thread)
 {
 	write(2, s, ft_strlen(s));
+	exit(EXIT_FAILURE);
+}
+
+void	thread_exception(const char *s, t_prog *app)
+{
+	int	i;
+
+	i = -1;
+	write(2, s, ft_strlen(s));
+	pthread_mutex_destroy(&app->print);
+	pthread_mutex_destroy(&app->dead);
+	pthread_mutex_destroy(&app->meal);
+	while (++i < app->philo_nbr)
+		pthread_mutex_destroy(&app->forks[i]);
 	exit(EXIT_FAILURE);
 }
