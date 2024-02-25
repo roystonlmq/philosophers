9e3a49131caa6c7cd64b23d53e8fdaa0e9cc1d14
roylee <roylee@iamagod.sg>
2024-02-25 12:50:57 +0800

    fix param order for init_app

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 18:07:20 by roylee            #+#    #+#             */
-/*   Updated: 2024/02/19 22:05:50 by roylee           ###   ########.fr       */
+/*   Updated: 2024/02/25 12:43:02 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ int	main(int argc, char **argv)
 	if (!(argc == 4 || argc == 5))
 		exception("./philo 5 800 200 200 [7]");
 	check_input(&app, argc, argv);
-	init_app(&app, argc, argv);
+	init_app(argc, argv, &app);
 	start(&app);
 }
