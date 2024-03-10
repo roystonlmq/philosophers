175f1c93232600f07fc76832444c1d7c2431b764
roylee <roylee@iamagod.sg>
2024-03-10 17:23:13 +0800

    update main

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 18:07:20 by roylee            #+#    #+#             */
-/*   Updated: 2024/02/25 12:43:02 by roylee           ###   ########.fr       */
+/*   Updated: 2024/02/25 13:29:07 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 int	main(int argc, char **argv)
 {
-	t_prog	app;
+	t_prog	*app;
 
 	if (!(argc == 4 || argc == 5))
 		exception("./philo 5 800 200 200 [7]");
-	check_input(&app, argc, argv);
-	init_app(argc, argv, &app);
-	start(&app);
+	check_input(argc, argv);
+	app = init_app(argc, argv);
+	start(app);
+	free_app(app);
 }
