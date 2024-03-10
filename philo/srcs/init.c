1126ecfc609d2588a1243afb7dcc01b14a844cd9
roylee <roylee@iamagod.sg>
2024-03-10 17:24:56 +0800

    remove * 1e3 for ttd, etc, as we're working in ms, last_meal get_time() instead of -1

--- philo/srcs/init.c
+++ philo/srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 12:43:39 by roylee            #+#    #+#             */
-/*   Updated: 2024/02/24 09:44:20 by roylee           ###   ########.fr       */
+/*   Updated: 2024/03/10 16:21:42 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,10 @@ static void	init_forks(t_prog *app)
 	philo_nbr = app->philo_nbr;
 	while (++i < philo_nbr)
 	{
-		pthread_mutex_init((t_mtx *)&app->forks[i], NULL);
-		app->forks[i].id = i;
+		pthread_mutex_init(&app->forks[i], NULL);
 		app->philos[i].left = &app->forks[(i + 1) % philo_nbr];
 		app->philos[i].right = &app->forks[i];
-		if (app->philos[i].id % 2 == 0)
+		if (i % 2 == 0)
 		{
 			app->philos[i].left = &app->forks[i];
 			app->philos[i].right = &app->forks[(i + 1) % philo_nbr];
@@ -33,6 +32,7 @@ static void	init_forks(t_prog *app)
 	}
 }
 
+// can try to init mutexes on app level instead of philo level
 static void	init_philos(t_prog *app)
 {
 	int	i;
@@ -43,35 +43,38 @@ static void	init_philos(t_prog *app)
 		app->philos[i].id = i + 1;
 		app->philos[i].app = app;
 		app->philos[i].eat_count = 0;
-		app->philos[i].last_meal = 0;
+		app->philos[i].last_meal = get_time();
 		app->philos[i].state = NONE;
+		
 	}
 }
 
 /*
 init_app
 
-init 3 mutexes for 3 potential race conditions:
-1. writing statuses
-2. checking if dead
-3. eating/meals
 */
-void	init_app(int ac, char **av, t_prog *app)
+t_prog	*init_app(int ac, char **av)
 {
+	t_prog *app;
+
+	app = ft_malloc(sizeof(t_prog));
+	app->threads = NULL;
 	app->philo_nbr = ft_atol(av[1]);
-	app->ttd = ft_atol(av[2]) * 1e3;
-	app->tte = ft_atol(av[3]) * 1e3;
-	app->tts = ft_atol(av[4]) * 1e3;
+	app->philos = ft_malloc(sizeof(t_philo) * app->philo_nbr);
+	init_philos(app);
+	app->forks = ft_malloc(sizeof(pthread_mutex_t) * app->philo_nbr);
+	init_forks(app);
+	app->ttd = ft_atol(av[2]);
+	app->tte = ft_atol(av[3]);
+	app->tts = ft_atol(av[4]);
+	app->start = get_time();
 	if (ac == 6)
 		app->eat_limit = ft_atol(av[5]);
 	else
 		app->eat_limit = -1;
 	app->end = 0;
-	app->philos = ft_malloc(sizeof(t_philo) * app->philo_nbr);
-	app->forks = ft_malloc(sizeof(t_mtx) * app->philo_nbr);
-	pthread_mutex_init(&app->print, NULL);
-	pthread_mutex_init(&app->dead, NULL);
 	pthread_mutex_init(&app->meal, NULL);
-	init_philos(app);
-	init_forks(app);
+	pthread_mutex_init(&app->dead, NULL);
+	pthread_mutex_init(&app->print, NULL);
+	return (app);
 }
