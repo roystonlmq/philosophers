7b239cad09d6d110698fa9c7d2dd1ed69319727f
roylee <roylee@iamagod.sg>
2024-03-10 17:23:53 +0800

    add free_app func

--- philo/srcs/free.c
+++ philo/srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 13:39:03 by roylee            #+#    #+#             */
-/*   Updated: 2024/02/19 21:53:07 by roylee           ###   ########.fr       */
+/*   Updated: 2024/03/10 16:21:03 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 void	free_app(t_prog *app)
 {
-	
+	int	i;
+
+	i = -1;
+	while (++i < app->philo_nbr)
+	{
+		pthread_mutex_destroy(&app->meal);
+		pthread_mutex_destroy(&app->dead);
+		pthread_mutex_destroy(&app->print);
+	}
+	i = -1;
+	while (++i < app->philo_nbr)
+		pthread_mutex_destroy(&app->forks[i]);
+	free(app->philos);
+	free(app->forks);
 }
