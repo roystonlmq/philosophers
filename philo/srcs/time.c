474289cb3f043c664fc6aa2c12ecc3d0e82c4595
roylee <roylee@iamagod.sg>
2024-03-10 17:26:33 +0800

    fix usleep to 500 only, to keep it consistent

--- philo/srcs/time.c
+++ philo/srcs/time.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 14:24:40 by roylee            #+#    #+#             */
-/*   Updated: 2024/02/19 20:52:56 by roylee           ###   ########.fr       */
+/*   Updated: 2024/03/10 16:06:05 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,10 @@ int		ft_usleep(long time)
 	long	start;
 
 	start = get_time();
-	while (get_time() - start < time)
-		usleep(time / 10);
+	while ((get_time() - start) < time)
+	{
+		// printf("now: %ld\n, tgt: %ld\n", get_time() - start, time);
+		usleep(500);
+	}
 	return (0);
 }
