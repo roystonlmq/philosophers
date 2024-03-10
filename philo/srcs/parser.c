b519183f2647006a05efa8e84610fe7d30ce9175
roylee <roylee@iamagod.sg>
2024-03-10 17:25:23 +0800

    using counter for while loop condition isntead

--- philo/srcs/parser.c
+++ philo/srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 12:43:35 by roylee            #+#    #+#             */
-/*   Updated: 2024/02/24 09:46:31 by roylee           ###   ########.fr       */
+/*   Updated: 2024/02/25 13:31:01 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,14 @@ check_input:
 ./philo	5	800			200			200				7
 			av[1]	av[2]		av[3]		av[4]			av[5]
 */
-void	check_input(t_prog *app, int ac, char **av)
+void	check_input(int ac, char **av)
 {
 	int		i;
-	(void)	app;
-	(void)	ac;
 
-	i = -1;
+	i = 0;
 	if (ft_atol(av[1]) <= 0)
 		exception("Invalid input\n");
-	while (av[++i])
+	while (++i < ac)
 	{
 		if (!ft_isnbr(av[i]))
 			exception("Invalid input\n");
