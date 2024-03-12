b190b1512663de0178176145f9ffb95257bac0e1
roylee <roystonlmq@gmail.com>
2024-03-12 21:40:00 +0800

    added philo nbr & eat limit to philo attr

--- philo/srcs/start.c
+++ philo/srcs/start.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 14:50:41 by roylee            #+#    #+#             */
-/*   Updated: 2024/03/10 19:57:37 by roylee           ###   ########.fr       */
+/*   Updated: 2024/03/12 21:39:56 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,45 +19,19 @@ static int	all_ate(t_philo *philos)
 
 	i = -1;
 	n = 0;
-	printf("all_ate func\n");
-	if (philos[0].app->eat_limit == -1)
+	if (philos[0].eat_limit == -1)
 		return (NOT_ALL_EATEN);
-	while (++i < philos[0].app->philo_nbr)
+	while (++i < philos[0].philo_nbr)
 	{
 		pthread_mutex_lock(&philos[i].app->meal);
-		if (philos[i].eat_count >= philos[i].app->eat_limit)
+		if (philos[i].eat_count >= philos[0].eat_limit)
 			n++;
 		pthread_mutex_unlock(&philos[i].app->meal);
 	}
-	if (philos[0].app->philo_nbr == n)
+	if (philos[0].philo_nbr == n)
 		return (ALL_ATE);
 	return (NOT_ALL_EATEN);
 }
-
-// static int isdead(t_philo *philos)
-// {
-// 	long	t_lastmeal;
-// 	int		i;
-
-// 	i = -1;
-// 	while (++i < philos[0].app->philo_nbr)
-// 	{
-// 		pthread_mutex_lock(&philos[i].app->meal);
-// 		t_lastmeal = get_time() - philos[i].last_meal;
-// 		if (t_lastmeal >= philos[i].app->ttd && philos[i].state != EAT)
-// 		{
-// 			logger(&philos[i], "died");
-// 			pthread_mutex_unlock(&philos[i].app->meal);
-// 			pthread_mutex_lock(&philos[i].app->dead);
-// 			philos[i].app->end = 1;
-// 			philos[i].state = DIED;
-// 			pthread_mutex_unlock(&philos[i].app->dead);
-// 			return (DIED);
-// 		}
-// 		pthread_mutex_unlock(&philos[i].app->meal);
-// 	}
-// 	return (ALIVE);
-// }
 
 /*
 checks if philo is dead or all philos ate
@@ -66,17 +40,16 @@ int		ft_state(t_philo *philo)
 {
 	long	t;
 
-	printf("ft_state func\n");
 	pthread_mutex_lock(&philo->app->meal);
 	t = get_time() - philo->last_meal;
 	if (t >= philo->app->ttd && philo->state != EAT)
 	{
+		pthread_mutex_unlock(&philo->app->meal);
 		logger(philo, "died");
 		pthread_mutex_lock(&philo->app->dead);
 		philo->app->end = 1;
 		philo->state = DIED;
 		pthread_mutex_unlock(&philo->app->dead);
-		pthread_mutex_unlock(&philo->app->meal);
 		return (DIED);
 	}
 	pthread_mutex_unlock(&philo->app->meal);
@@ -91,10 +64,8 @@ void	*monitor(void *arg)
 	philos = (t_philo *)arg;
 	while (1)
 	{
-		printf("monitor func\n");
-		fflush(stdout);
 		i = -1;
-		while (++i < philos[0].app->philo_nbr)
+		while (++i < philos[0].philo_nbr)
 		{
 			if (ft_state(&philos[i]) == DIED)
 				return (arg);
@@ -114,7 +85,6 @@ int		check_end(t_philo *philo)
 {
 	int	end;
 
-	// printf("check_dead func\n");
 	pthread_mutex_lock(&philo->app->dead);
 	end = philo->app->end;
 	pthread_mutex_unlock(&philo->app->dead);
@@ -133,7 +103,6 @@ void	*start_routine(void *arg)
 		eat(philo);
 		psleep(philo);
 		think(philo);
-		// printf("start_routine func\n");
 	}
 	return (arg);
 }
