21d40c55b0ecb4824de7663d3fd08b97b0cdbc54
roylee <roylee@iamagod.sg>
2024-03-16 13:34:46 +0800

    add destroy for state_lock

--- philo/srcs/free.c
+++ philo/srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 13:39:03 by roylee            #+#    #+#             */
-/*   Updated: 2024/03/12 23:49:12 by roylee           ###   ########.fr       */
+/*   Updated: 2024/03/16 13:33:49 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ void	free_app(t_prog *app)
 	pthread_mutex_destroy(&app->print);
 	i = -1;
 	while (++i < app->philo_nbr)
+	{
 		pthread_mutex_destroy(&app->forks[i]);
+		pthread_mutex_destroy(&app->philos[i].state_lock);
+	}
 	free(app->philos);
 	free(app->forks);
 	free(app);
