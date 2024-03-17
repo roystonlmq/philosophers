447b288ac1c340ec1ad77c2f702d604b79749803
roylee <roylee@iamagod.sg>
2024-03-17 18:15:12 +0800

    change set_end param, remove observer thread

--- philo/srcs/start.c
+++ philo/srcs/start.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 14:50:41 by roylee            #+#    #+#             */
-/*   Updated: 2024/03/17 13:46:57 by roylee           ###   ########.fr       */
+/*   Updated: 2024/03/17 18:14:55 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,12 @@ static void	get_philo_ate_cnt(t_philo *philo, int *n)
 	pthread_mutex_unlock(&philo->app->meal);
 }
 
-void	*monitor(void *arg)
+void	monitor(t_philo *philos)
 {
-	t_philo	*philos;
 	int		i;
 	int		n;
 	int		e;
 
-	philos = (t_philo *)arg;
 	e = 1;
 	while (e)
 	{
@@ -43,12 +41,11 @@ void	*monitor(void *arg)
 			if (philos[0].app->philo_nbr == n)
 				e = 0;
 			if (e == 0)
-				set_end(philos[0].app);
+				set_end(&philos[0]);
 		}
 	}
-	return (arg);
+	return ;
 }
-
 void	*start_routine(void *arg)
 {
 	t_philo	*philo;
@@ -67,7 +64,6 @@ void	*start_routine(void *arg)
 
 void	start(t_prog *app)
 {
-	pthread_t	watch;
 	int			i;
 
 	i = -1;
@@ -75,10 +71,7 @@ void	start(t_prog *app)
 		if (pthread_create(&app->philos[i].tid, NULL, &start_routine, \
 				&app->philos[i]) != 0)
 			thread_exception(THD_CREAT_FAIL, app);
-	if (pthread_create(&watch, NULL, &monitor, app->philos) != 0)
-		thread_exception(THD_CREAT_FAIL, app);
-	if (pthread_join(watch, NULL) != 0)
-		thread_exception(THD_JOIN_FAIL, app);
+	monitor(app->philos);
 	i = -1;
 	while (++i < app->philo_nbr)
 		if (pthread_join(app->philos[i].tid, NULL) != 0)
