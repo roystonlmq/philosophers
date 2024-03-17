01ff3187fed9ecb3d3db5cd571f53fcb2cf78cf8
roylee <roylee@iamagod.sg>
2024-03-17 13:22:26 +0800

    increase delay for even philos to start eating

--- philo/srcs/start.c
+++ philo/srcs/start.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 14:50:41 by roylee            #+#    #+#             */
-/*   Updated: 2024/03/17 01:07:34 by roylee           ###   ########.fr       */
+/*   Updated: 2024/03/17 13:21:00 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ void	*start_routine(void *arg)
 
 	philo = (t_philo *)arg;
 	if (philo->id % 2 == 0)
-		ft_usleep(1);
+		ft_usleep(100);
 	while (check_end(philo) == 0)
 	{
 		eat(philo);
