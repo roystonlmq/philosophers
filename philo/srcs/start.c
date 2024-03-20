43af1adea248e13dafa09fc881ef1c398670113c
roylee <roylee@iamagod.sg>
2024-03-20 19:15:36 +0800

    add static func to break up start, force odd philos to delay before starting

--- philo/srcs/start.c
+++ philo/srcs/start.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 14:50:41 by roylee            #+#    #+#             */
-/*   Updated: 2024/03/20 18:22:25 by roylee           ###   ########.fr       */
+/*   Updated: 2024/03/20 19:09:28 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	*monitor(void *data)
 	int		n;
 
 	philos = (t_philo *)data;
-	while (philos[0].app->end == 0)
+	while (check_end(&philos[0]) == 0)
 	{
 		i = -1;
 		n = 0;
@@ -50,13 +50,39 @@ void	*start_routine(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
-	while (philo->app->ready == NOT_RDY)
+	while (1)
+	{
+		pthread_mutex_lock(&philo->app->thds_rdy);
+		if (philo->app->ready == READY)
+		{
+			pthread_mutex_unlock(&philo->app->thds_rdy);
+			break ;
+		}
+		pthread_mutex_unlock(&philo->app->thds_rdy);
 		continue ;
-	if (philo->id % 2 == 0)
+	}
+	if (philo->id % 2 != 0)
 		ft_sleep(1);
 	while (check_end(philo) == 0)
 		eat_slp_think(philo);
 	return (arg);
+}
+
+static void	app_ready(t_prog *app)
+{
+	int	i;
+
+	app->start = get_current_ms();
+	i = -1;
+	while (++i < app->philo_nbr)
+	{
+		app->philos[i].philo_start = app->start;
+		app->philos[i].last_meal = app->start;
+	}
+	pthread_mutex_unlock(&app->time);
+	pthread_mutex_lock(&app->thds_rdy);
+	app->ready = READY;
+	pthread_mutex_unlock(&app->thds_rdy);
 }
 
 int	start(t_prog *app)
@@ -69,16 +95,8 @@ int	start(t_prog *app)
 		if (pthread_create(&app->philos[i].tid, NULL, &start_routine, \
 				&app->philos[i]) != 0)
 			return (thread_exception(THD_CREAT_FAIL, app));
-	i = -1;
-	app->start = get_current_ms();
-	while (++i < app->philo_nbr)
-	{
-		app->philos[i].philo_start = app->start;
-		app->philos[i].last_meal = app->start;
-	}
-	app->ready = READY;
-	while (app->ready == NOT_RDY)
-		continue ;
+	pthread_mutex_lock(&app->time);
+	app_ready(app);
 	if (pthread_create(&watch, NULL, &monitor, app->philos) != 0)
 		return (thread_exception(THD_CREAT_FAIL, app));
 	if (pthread_join(watch, NULL) != 0)
