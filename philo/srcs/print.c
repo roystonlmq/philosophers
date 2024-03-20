7ffe98f1fe3d1eff900809a5566dd8eb02692a51
roylee <roylee@iamagod.sg>
2024-03-20 19:15:06 +0800

    mutex lock app->start & move eat_count after finsihed eating

--- philo/srcs/print.c
+++ philo/srcs/print.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 14:57:50 by roylee            #+#    #+#             */
-/*   Updated: 2024/03/19 23:29:35 by roylee           ###   ########.fr       */
+/*   Updated: 2024/03/20 19:05:38 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 
 void	logger(t_philo *philo, char *s)
 {
+	long	t;
+
+	pthread_mutex_lock(&philo->app->time);
+	t = get_current_ms() - philo->app->start;
+	pthread_mutex_unlock(&philo->app->time);
 	pthread_mutex_lock(&philo->app->print);
 	if (check_end(philo) == 1)
 	{
 		pthread_mutex_unlock(&philo->app->print);
 		return ;
 	}
-	printf("%ld %d %s\n", get_current_ms() - philo->app->start, philo->id, s);
+	printf("%ld %d %s\n", t, philo->id, s);
 	pthread_mutex_unlock(&philo->app->print);
 }
 
@@ -60,12 +65,14 @@ void	eat_slp_think(t_philo *philo)
 		return (one_philo(philo));
 	pthread_mutex_lock(philo->second);
 	logger(philo, "has taken a fork");
+	logger(philo, "is eating");
 	pthread_mutex_lock(&philo->app->meal);
-	philo->eat_count++;
 	philo->last_meal = get_current_ms();
 	pthread_mutex_unlock(&philo->app->meal);
-	logger(philo, "is eating");
 	ft_sleep(philo->app->tte);
+	pthread_mutex_lock(&philo->app->meal);
+	philo->eat_count++;
+	pthread_mutex_unlock(&philo->app->meal);
 	pthread_mutex_unlock(philo->first);
 	pthread_mutex_unlock(philo->second);
 	slp_think(philo);
