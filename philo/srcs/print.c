83d5adb5b638b9f523ffc1c8b0414ad7a2ba48f9
roylee <roystonlmq@gmail.com>
2024-03-23 00:13:21 +0800

    remove time mutex

--- philo/srcs/print.c
+++ philo/srcs/print.c
@@ -6,27 +6,27 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 14:57:50 by roylee            #+#    #+#             */
-/*   Updated: 2024/03/21 19:58:26 by roylee           ###   ########.fr       */
+/*   Updated: 2024/03/23 00:05:56 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	logger(t_philo *philo, char *s)
+void	logger(t_philo *philo, char *s, char *s2)
 {
 	long	t;
 
 	pthread_mutex_lock(&philo->app->print);
-	pthread_mutex_lock(&philo->app->time);
 	if (check_end(philo) == 1)
 	{
-		pthread_mutex_unlock(&philo->app->time);
 		pthread_mutex_unlock(&philo->app->print);
 		return ;
 	}
-	t = get_current_ms() - philo->app->start;
-	pthread_mutex_unlock(&philo->app->time);
-	printf("%ld %d %s\n", t, philo->id, s);
+	t = get_current_ms() - philo->philo_start;
+	if (!s2)
+		printf("%ld %d %s\n", t, philo->id, s);
+	if (s && s2)
+		printf("%ld %d %s\n%ld %d %s\n", t, philo->id, s, t, philo->id, s2);
 	pthread_mutex_unlock(&philo->app->print);
 }
 
@@ -35,11 +35,9 @@ void	dead_logger(t_philo *philo, char *s)
 	long	t;
 
 	pthread_mutex_lock(&philo->app->print);
-	pthread_mutex_lock(&philo->app->time);
-	t = get_current_ms() - philo->app->start;
+	t = get_current_ms() - philo->philo_start;
 	set_end(philo);
 	printf("%ld %d %s\n", t, philo->id, s);
-	pthread_mutex_unlock(&philo->app->time);
 	pthread_mutex_unlock(&philo->app->print);
 }
 
@@ -48,9 +46,9 @@ static void	slp_think(t_philo *philo)
 	long	tts;
 
 	tts = philo->app->tts;
-	logger(philo, "is sleeping");
+	logger(philo, "is sleeping", 0);
 	ft_sleep(tts);
-	logger(philo, "is thinking");
+	logger(philo, "is thinking", 0);
 }
 
 static void	one_philo(t_philo *philo)
@@ -63,12 +61,11 @@ static void	one_philo(t_philo *philo)
 void	eat_slp_think(t_philo *philo)
 {
 	pthread_mutex_lock(philo->first);
-	logger(philo, "has taken a fork");
+	logger(philo, "has taken a fork", 0);
 	if (philo->app->philo_nbr == 1)
 		return (one_philo(philo));
 	pthread_mutex_lock(philo->second);
-	logger(philo, "has taken a fork");
-	logger(philo, "is eating");
+	logger(philo, "has taken a fork", "is eating");
 	pthread_mutex_lock(&philo->app->meal);
 	philo->last_meal = get_current_ms();
 	pthread_mutex_unlock(&philo->app->meal);
