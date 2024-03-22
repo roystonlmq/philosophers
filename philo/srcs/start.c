5de0061a6d43f80ef49f4b10e9ad246a54b38bed
roylee <roystonlmq@gmail.com>
2024-03-23 00:13:07 +0800

    add int return val for philo_ate_cnt

--- philo/srcs/start.c
+++ philo/srcs/start.c
@@ -6,20 +6,24 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 14:50:41 by roylee            #+#    #+#             */
-/*   Updated: 2024/03/21 19:54:58 by roylee           ###   ########.fr       */
+/*   Updated: 2024/03/23 00:12:27 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	get_philo_ate_cnt(t_philo *philo, int *n)
+static int	get_philo_ate_cnt(t_philo *philo, int *n)
 {
 	pthread_mutex_lock(&philo->app->meal);
 	if (philo->eat_count >= philo->app->eat_limit)
 		(*n)++;
 	pthread_mutex_unlock(&philo->app->meal);
 	if (philo->app->philo_nbr == *n)
-		return (set_end(philo));
+	{
+		set_end(philo);
+		return (1);
+	}
+	return (0);
 }
 
 void	*monitor(void *data)
@@ -27,19 +31,22 @@ void	*monitor(void *data)
 	t_philo	*philos;
 	int		i;
 	int		n;
+	int		flag;
 
 	philos = (t_philo *)data;
-	while (check_end(&philos[0]) == 0)
+	flag = 1;
+	while (flag)
 	{
 		i = -1;
 		n = 0;
-		while (++i < philos[0].app->philo_nbr)
+		while (++i < philos[0].app->philo_nbr && flag == 1)
 		{
-			if (philos[0].app->end == 1 || ft_state(&philos[i]) == DIED)
-				break ;
+			if (ft_state(&philos[i]) == DIED)
+				flag = 0;
 			if (philos[0].app->eat_limit == -1)
 				continue ;
-			get_philo_ate_cnt(&philos[i], &n);
+			if (get_philo_ate_cnt(&philos[i], &n) == 1)
+				flag = 0;
 		}
 	}
 	return (data);
@@ -79,7 +86,6 @@ static void	app_ready(t_prog *app)
 		app->philos[i].philo_start = app->start;
 		app->philos[i].last_meal = app->start;
 	}
-	pthread_mutex_unlock(&app->time);
 	pthread_mutex_lock(&app->thds_rdy);
 	app->ready = READY;
 	pthread_mutex_unlock(&app->thds_rdy);
@@ -95,7 +101,6 @@ int	start(t_prog *app)
 		if (pthread_create(&app->philos[i].tid, NULL, &start_routine, \
 				&app->philos[i]) != 0)
 			return (thread_exception(THD_CREAT_FAIL, app));
-	pthread_mutex_lock(&app->time);
 	app_ready(app);
 	if (pthread_create(&watch, NULL, &monitor, app->philos) != 0)
 		return (thread_exception(THD_CREAT_FAIL, app));
